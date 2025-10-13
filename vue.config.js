0fbd1a5956ed42705a46e345d4d2c7d6a776140e
dewfire <3151716799@qq.com>
2025-10-13 14:51:53 +0800

    添加title

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 添加页面标题配置
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'DewShop - 您的在线购物平台'
+    }
+  }
 })
