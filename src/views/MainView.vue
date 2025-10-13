abcc88a2c2b0708f7d07741171506e35a6410b9b
dewfire <3151716799@qq.com>
2025-10-13 15:36:19 +0800

    简单完善一下顶部导航条的欢迎部分

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>
-    <h1>Hello MainView</h1>
+  <div class="mainview">
+    <div class="topbar">
+      <div class="container clearfix">
+        <div class="welcome leftfix">
+          <span class="hello">DEWshop欢迎您</span>
+          <span>
+            <a href="#">登录</a>
+            <a href="#">注册</a>
+          </span>
+        </div>
+        <div class="nav rightfix"></div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 
 </script>
 <style>
+.container{
+  width: 1190px;
+  margin: 0 auto;
+}
 
+.topbar{
+  height:30px;
+  background-color: #ECECEC;
+}
+.welcome{
+  height: 30px;
+  line-height: 30px;
+  color: #666;
+  font-size: 12px;
+}
+.hello{
+  margin-right: 28px;
+}
+.nav{
+  height: 30px;
+  line-height: 30px;
+}
 </style>
