10ba22ddd20a7afa9a49079a0985bbf220bb085c
dewfire <3151716799@qq.com>
2025-10-31 09:57:06 +0800

    安装了element-plus

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 // import store from './store'
+// 引入element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import './assets/reset.css'
 
 
 
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(ElementPlus).mount('#app')
